a85f75266c517da79fb184a59e76603aec593dff
Rohit Bapat <rbapat@tibco.com>
2018-01-19 12:58:30 +0530

    Updated Deactivation Process

--- working/code/bpm/com.sfc.fosimpl.deactivation/.forms/gwt/comsfcfosImplremoval_tobe/comsfcfosImplremoval_tobeRemoval/SelectContractorAgency/SelectContractorAgency.gwt.js
+++ working/code/bpm/com.sfc.fosimpl.deactivation/.forms/gwt/comsfcfosImplremoval_tobe/comsfcfosImplremoval_tobeRemoval/SelectContractorAgency/SelectContractorAgency.gwt.js
@@ -2,9 +2,9 @@
 
 if (typeof(tibcoforms) == 'undefined') tibcoforms = new Object();
 if (typeof(tibcoforms.formCode) == 'undefined') tibcoforms.formCode = new Object();
-tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q'] = new Object();
-tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q']['defineActions'] = function() {
-var fc = tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q'];
+tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw'] = new Object();
+tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw']['defineActions'] = function() {
+var fc = tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw'];
     fc['rule_cancel'] = function(formId, context, thisObj) {
 	   try {
 			tibco.forms.Util.handleScriptAction.call(thisObj, formId, context, thisObj, "cancel", "cancel", fc['action_cancel']);
@@ -60,10 +60,10 @@ var fc = tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q'];
         return "TIBCO Forms 4.1.0 V17 compliant";
     }
 };
-tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q']['defineActions']();
+tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw']['defineActions']();
 
-tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q']['defineValidations'] = function() {
-var fc = tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q'];
+tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw']['defineValidations'] = function() {
+var fc = tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw'];
 	
 	
 	
@@ -114,4 +114,4 @@ var fc = tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q'];
 	   
 	   
 };
-tibcoforms.formCode['_DF8fQPw7EeeupNwu8S8N4Q']['defineValidations']();
+tibcoforms.formCode['_vmdKAPzlEeehkqoKISnbpw']['defineValidations']();
